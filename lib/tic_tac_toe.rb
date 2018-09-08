0a55735a63cc0cd977aa4430454c31d91442d145
Mary Lark <mary@marylark.com>
2018-09-08 21:50:09 +0000

    automatically backed up by learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,62 @@
+class TicTacToe
+  WIN_COMBINATIONS = [
+                        [0,1,2],
+                        [3,4,5],
+                        [6,7,8],
+                        [0,3,6],
+                        [1,4,7],
+                        [2,5,8],
+                        [0,4,8],
+                        [2,4,6]
+  ]
+  
+  def initialize
+    @board - [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def draw?
+  end
+  
+  def full?
+  end
+
+  def over?
+  end
+  
+  def position_taken?
+  end
+
+  def valid_move?
+  end
+
+  def won?
+  end
+  
+
+  def current_player
+  end
+  
+  def display_board
+  end
+
+  def input_to_index
+  end
+
+  def move
+  end
+
+  def play
+  end
+  
+  def turn_count
+  end
+
+  def turn
+  end
+
+  def winner
+  end
+  
+  
+  
+end
