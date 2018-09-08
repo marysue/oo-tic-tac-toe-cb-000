6fb67c6a91124d74d8cd68cb71993a559e6fb58f
Mary Lark <mary@marylark.com>
2018-09-08 23:11:31 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -103,11 +103,12 @@ end #end won?
   end
 
   def input_to_index(input)
-    if index == -1
-      puts "Invalid entry."
-    elsif !valid_move?(index)
-      puts "Space taken. Choose again."
-      index = -1
+    #returns 0 if not a float or int
+    index = input.to_i
+    if index.between?(1,9)
+      return (index - 1)
+    else
+      return -1
     end
   end
 
