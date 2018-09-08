1acce96792b84014f823b25a3403d5613a0bb1b7
Mary Lark <mary@marylark.com>
2018-09-08 21:57:37 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,7 +40,28 @@ class TicTacToe
   end
 
   def won?
-  end
+    #won? : returns winning combination or false if no winning comb
+    #for each row, column and diagnonal combination
+    @WIN_COMBINATIONS.each do | win_combination |
+      #grab the winning combination of indices we are looking for
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+
+      #extract the value of these winning indices from the board
+      position_1 = @board[win_index_1]
+      position_2 = @board[win_index_2]
+      position_3 = @board[win_index_3]
+
+      #check if either team has won
+      if (position_1 == "X" && position_2 == "X" && position_3 == "X") ||
+         (position_1 == "O" && position_2 == "O" && position_3 == "O")
+         #we break out of here if we have a winning row, col, or diagnonal
+         return win_combination
+      end #end if
+  end #end do while
+  return false
+end #end won?
 
 
   def current_player
